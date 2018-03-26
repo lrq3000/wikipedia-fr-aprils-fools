ab25d459c9e35e1e916ab24e27429b00990a07e7
Stephen L <lrq3000@gmail.com>
2018-03-26 19:50:53 +0200

    bump version
    Signed-off-by: Stephen L. <lrq3000@gmail.com>

--- minitel-anim.js
+++ minitel-anim.js
@@ -1,6 +1,6 @@
 /* Minitel animation for MediaWiki + CSS dynamic management
  * by LRQ3000
- * v2.1.6
+ * v2.1.7
  * Released under MIT license.
  * Usage: either simply include this script at the header:
  *
